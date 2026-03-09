e01541dd477e7ec41517c479f5707c712a6d1181
Raviraj Kumar <ravirajkumarstudy@gmail.com>
2026-03-09 18:23:03 +0530

    /

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -343,7 +343,7 @@ resource "aws_instance" "app" {
   subnet_id              = module.vpc.public_subnet_ids[0]
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
-  key_name               = var.ec2_key_pair_name
+  key_name               = var.ec2_key_pair_name != "" ? var.ec2_key_pair_name : null
 
   root_block_device {
     volume_size           = 30
